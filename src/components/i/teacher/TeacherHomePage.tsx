b9baf9a07f8aef3f38ae27aad6567fa07e608b56
hagoel-git <danilo.guk1@gmail.com>
2024-05-03 23:31:36 +0300

    Created TeacherHomePage

--- src/components/i/teacher/TeacherHomePage.tsx
+++ src/components/i/teacher/TeacherHomePage.tsx
@@ -1,10 +1,36 @@
 'use client'
 
+import {Card, List} from "antd";
+
 export default function TeacherHomePage() {
+    // todo: fetch subjects and grades from the server
+    const subjects = [
+        "Math",
+        "Physics",
+        "Chemistry",
+        "Biology",
+        "History",
+        "Geography",
+        "Literature",
+        "English",
+        "Ukrainian",
+        "Russian",
+        "German",
+    ];
     return (
         <div>
-            <h1>Головна сторінка для вчителя</h1>
-            <p>Вміст.</p>
+            <h1>Головна сторінка для викладача</h1>
+            <List pagination={{pageSize: 8}}
+                  header={<h2>Subjects</h2>}
+                  split={false}
+                  grid={{gutter: 16, column: 4}}
+                  dataSource={subjects}
+                  renderItem={item => (
+                      <List.Item>
+                          <Card title={item}>Card content</Card>
+                      </List.Item>
+                  )}
+            />
         </div>
     )
 }
