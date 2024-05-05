af6d14efcd7a971469e7c0d44b92bf71b2aa111e
pupkin-afk <ma5400026@gmail.com>
2024-05-05 07:27:20 +0300

    added loader spinner & loader error

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { Layout } from "antd";
+import { Button, Layout, Result } from "antd";
 import { Content } from "antd/es/layout/layout";
 
 import { Spin } from "antd";
 import { LoadingOutlined } from '@ant-design/icons';
 import { useEffect, useRef, useState } from "react";
 import { useAppStore } from "@/stores/appStore";
+import { ProfileService } from "@/services/profile.service";
 
 export default function ILayout({
   children,
@@ -15,21 +16,28 @@ export default function ILayout({
   const isLoading = useAppStore(state => state.isLoading);
   const [isAppLoading, setAppLoading] = useState(true);
 
+  const [isFailedToLoad, setIsFailedToLoad] = useState(false);
+
   const initialized = useRef(false);
   useEffect(() => {
     if (!initialized.current) {
       initialized.current = true;
 
-      setTimeout(() => {
+      ProfileService.getProfile().then(response => {
         setAppLoading(false);
-      }, 3000);
+        if (!response.success) {
+          setIsFailedToLoad(true);
+          return;
+        }
+
+      })
     }
   }, []);
 
   return (
       <Layout>
           <Content style={{margin: "15px 16px"}}>
-          {!isAppLoading ? 
+          {(!isAppLoading && !isFailedToLoad) ? 
             <div style={{background: "#141414", borderRadius: 10, padding: 24, minHeight: 360}}>
               {children}
             </div>
@@ -40,6 +48,24 @@ export default function ILayout({
             fullscreen
             tip={<h1>Завантаження...</h1>}
           />
+
+          {isFailedToLoad ? 
+            <Result
+              status="error"
+              title="Не вдалося завантажити сайт"
+              subTitle="Провірте підключення до інтернету або статус серверу."
+              extra={[
+                <Button
+                  type="primary"
+                  key="tryagain"
+                  onClick={() => window.location.href = window.location.href}
+                >
+                  Спробувати ще раз
+                </Button>,
+              ]}
+            />
+          : null}
+
           </Content>
       </Layout>
   );
