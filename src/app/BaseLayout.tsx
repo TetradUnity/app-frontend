18477e38dc5a3930481bf942ac50eddcea9d5c15
hagoel-git <danilo.guk1@gmail.com>
2024-05-10 23:11:33 +0300

    fixed FOUC bug

--- src/app/BaseLayout.tsx
+++ src/app/BaseLayout.tsx
@@ -1,28 +1,31 @@
 'use client'
-import { ConfigProvider, Layout, theme } from "antd";
+import {ConfigProvider, Layout, theme} from "antd";
 import Header from "@/components/Header";
+import {AntdRegistry} from "@ant-design/nextjs-registry";
 
 export default function BaseLayout({
-    children,
-  }: Readonly<{
+                                       children,
+                                   }: Readonly<{
     children: React.ReactNode;
-  }>) {
+}>) {
     return (
-        <ConfigProvider
-            theme={{
-            algorithm: theme.darkAlgorithm,
-            token: {
-                "colorPrimary": "#5e35b1",
-                "colorInfo": "#ab7ae0",
-                "colorBgBase": "#0A0A0A",
-                "fontSize": 17,
-            },
-            hashed: false
-        }}>
-            <Layout style={{minHeight: '100vh'}}>
-                <Header />
-                {children}
-            </Layout>
-        </ConfigProvider>
+        <AntdRegistry>
+            <ConfigProvider
+                theme={{
+                    algorithm: theme.darkAlgorithm,
+                    token: {
+                        "colorPrimary": "#5e35b1",
+                        "colorInfo": "#ab7ae0",
+                        "colorBgBase": "#0A0A0A",
+                        "fontSize": 17,
+                    },
+                    hashed: false
+                }}>
+                <Layout style={{minHeight: '100vh'}}>
+                    <Header/>
+                    {children}
+                </Layout>
+            </ConfigProvider>
+        </AntdRegistry>
     );
-  }
+}
