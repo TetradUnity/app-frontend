ad065ea44726cec1bd516fc7869b1e5a5fe4f04a
hagoel-git <danilo.guk1@gmail.com>
2024-05-12 01:11:08 +0300

    fixed always visible Spin div

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,73 +1,63 @@
 'use client'
-import { Button, Layout, Result } from "antd";
-import { Content } from "antd/es/layout/layout";
+import {Button, Layout, Result} from "antd";
+import {Content} from "antd/es/layout/layout";
 
-import { Spin } from "antd";
-import { LoadingOutlined } from '@ant-design/icons';
-import { CSSProperties, useEffect, useState } from "react";
-import { useAppStore } from "@/stores/appStore";
-import { UserService } from "@/services/user.service";
-import { useProfileStore } from "@/stores/profileStore";
-
-const contentStyle: CSSProperties = {
-
-};
+import {Spin} from "antd";
+import {LoadingOutlined} from '@ant-design/icons';
+import {useEffect, useState} from "react";
+import {UserService} from "@/services/user.service";
+import {useProfileStore} from "@/stores/profileStore";
 
 export default function ILayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                    children,
+                                }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  const isLoading = useAppStore(state => state.isLoading);
-  const [isAppLoading, setAppLoading] = useState(true);
+    const [isAppLoading, setAppLoading] = useState(true);
 
-  const [isFailedToLoad, setIsFailedToLoad] = useState(false);
+    const [isFailedToLoad, setIsFailedToLoad] = useState(false);
 
-  const updateProfileStore = useProfileStore(selector => selector.updateProfile);
+    const updateProfileStore = useProfileStore(selector => selector.updateProfile);
 
-  useEffect(() => {
-    UserService.getProfile().then(response => {
-      updateProfileStore(response.data);
-      setAppLoading(false);
+    useEffect(() => {
+        UserService.getProfile().then(response => {
+            updateProfileStore(response.data);
+            setAppLoading(false);
 
-      if (!response.success) {
-        setIsFailedToLoad(true);
-        return;
-      }
-    })
-  }, []);
+            if (!response.success) {
+                setIsFailedToLoad(true);
+                return;
+            }
+        })
+    }, []);
 
-  return (
-      <Layout style={{flex: 1}}>
-          <Content>
-            {(!isAppLoading && !isFailedToLoad) ? 
-                  children
-              : null}
-          <Spin
-            indicator={<LoadingOutlined style={{ fontSize: 60 }}/>}
-            spinning={isAppLoading || isLoading}
-            fullscreen
-            tip={<h1>Завантаження...</h1>}
-          />
-
-          {isFailedToLoad ? 
-            <Result
-              status="error"
-              title="Не вдалося завантажити сайт"
-              subTitle="Провірте підключення до інтернету або статус серверу."
-              extra={[
-                <Button
-                  type="primary"
-                  key="tryagain"
-                  onClick={() => window.location.href = window.location.href}
-                >
-                  Спробувати ще раз
-                </Button>,
-              ]}
-            />
-          : null}
-
-          </Content>
-      </Layout>
-  );
+    return (
+        <Layout style={{flex: 1}}>
+            <Content>
+                {!isAppLoading && !isFailedToLoad && children}
+                {isAppLoading && <Spin
+                    indicator={<LoadingOutlined style={{fontSize: 60}}/>}
+                    spinning={true}
+                    fullscreen
+                    tip={<h1>Завантаження...</h1>}
+                />}
+                {isFailedToLoad &&
+                    <Result
+                        status="error"
+                        title="Не вдалося завантажити сайт"
+                        subTitle="Провірте підключення до інтернету або статус серверу."
+                        extra={[
+                            <Button
+                                type="primary"
+                                key="tryagain"
+                                onClick={() => window.location.reload()}
+                            >
+                                Спробувати ще раз
+                            </Button>,
+                        ]}
+                    />
+                }
+            </Content>
+        </Layout>
+    );
 }
