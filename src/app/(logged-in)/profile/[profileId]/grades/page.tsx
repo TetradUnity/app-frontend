a9ef5a2ef54c6d9cf1ae70b233e5260b209271dc
hagoel-git <danilo.guk1@gmail.com>
2024-05-25 01:43:41 +0300

    hide /grades from another students

--- src/app/(logged-in)/profile/[profileId]/grades/page.tsx
+++ src/app/(logged-in)/profile/[profileId]/grades/page.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 import styles from "./styles.module.css"
 import {SearchOutlined} from "@ant-design/icons";
 import Link from "next/link";
+import {useQueryProfileStore} from "@/stores/queryProfileStore";
 
 const items = [{
     label: "subjectName",
@@ -35,10 +36,11 @@ const items = [{
 }]
 
 export default function ProfileGrades() {
-
     const [sort, setSort] = useState("name");
     const [sortOrder, setSortOrder] = useState("asc");
     const [searchText, setSearchText] = useState("");
+
+    const profile = useQueryProfileStore();
 
     const onChangeSortOrder = (e: RadioChangeEvent) => {
         setSortOrder(e.target.value)
@@ -49,57 +51,68 @@ export default function ProfileGrades() {
     const sortingBlock = document.getElementById("sorting-block")
     let sortingBlockWidth = sortingBlock !== null ? sortingBlock.offsetWidth : 0
     return (
-        <div style={{
-            display: "grid",
-            gap: 'var(--gap)',
-            gridTemplateColumns: "auto 1fr",
-            gridTemplateRows: "auto 1fr",
-            gridTemplateAreas: `
+        <>
+            {profile.isMe ?
+                <div style={{
+                    display: "grid",
+                    gap: 'var(--gap)',
+                    gridTemplateColumns: "auto 1fr",
+                    gridTemplateRows: "auto 1fr",
+                    gridTemplateAreas: `
                 "sidebar search"
                 "sidebar content"
                 `,
-        }}>
-            <Space id="sorting-block" style={{
-                display: "block",
-                position: "sticky",
-                top: "calc(56px + var(--gap))",
-                background: 'var(--foreground)',
-                borderRadius: 8,
-                padding: "12px 0px",
-                width: "max-content",
-                height: "max-content",
-                zIndex: 50,
-                gridArea: "sidebar",
-            }}>
-                <Divider style={{margin: 0}} plain orientation="left">Сортування предметів</Divider>
-                <Radio.Group value={sort} onChange={onChangeSort} style={{margin: "8px 0", padding: "0 12px"}}>
-                    <Space direction="vertical" size="small">
-                        <Radio value={"name"}>По назві</Radio>
-                        <Radio value={"lastGrade"}>По даті останньої оцінки</Radio>
-                        <Radio value={"enrollDate"}>По даті приєднання</Radio>
-                        <Radio value={"studentCount"}>По кількості студентів</Radio>
+                }}>
+                    <Space id="sorting-block" style={{
+                        display: "block",
+                        position: "sticky",
+                        top: "calc(56px + var(--gap))",
+                        background: 'var(--foreground)',
+                        borderRadius: 8,
+                        padding: "12px 0px",
+                        width: "max-content",
+                        height: "max-content",
+                        zIndex: 50,
+                        gridArea: "sidebar",
+                    }}>
+                        <Divider style={{margin: 0}} plain orientation="left">Сортування предметів</Divider>
+                        <Radio.Group value={sort} onChange={onChangeSort} style={{margin: "8px 0", padding: "0 12px"}}>
+                            <Space direction="vertical" size="small">
+                                <Radio value={"name"}>По назві</Radio>
+                                <Radio value={"lastGrade"}>По даті останньої оцінки</Radio>
+                                <Radio value={"enrollDate"}>По даті приєднання</Radio>
+                                <Radio value={"studentCount"}>По кількості студентів</Radio>
+                            </Space>
+                        </Radio.Group>
+                        <Divider style={{margin: 0}}></Divider>
+                        <Radio.Group value={sortOrder} onChange={onChangeSortOrder}
+                                     style={{margin: "8px 0", padding: "0 12px"}}>
+                            <Space direction="vertical" size="small">
+                                <Radio value={"asc"}>За зростанням</Radio>
+                                <Radio value={"desc"}>За спаданням</Radio>
+                            </Space>
+                        </Radio.Group>
                     </Space>
-                </Radio.Group>
-                <Divider style={{margin: 0}}></Divider>
-                <Radio.Group value={sortOrder} onChange={onChangeSortOrder}
-                             style={{margin: "8px 0", padding: "0 12px"}}>
-                    <Space direction="vertical" size="small">
-                        <Radio value={"asc"}>За зростанням</Radio>
-                        <Radio value={"desc"}>За спаданням</Radio>
-                    </Space>
-                </Radio.Group>
-            </Space>
-            <Flex vertical gap='var(--gap)' style={{maxWidth: 1200 - sortingBlockWidth}}>
-                <Input placeholder="Пошук предметів" prefix={<SearchOutlined/>}
-                       onChange={e => setSearchText(e.target.value)}
-                       style={{position: "relative", flexGrow: 1, gridArea: "search",}}></Input>
-                {/* todo when subjects will be in api */}
-                <Flex vertical style={{background: 'var(--foreground)', borderRadius: 8, gridArea: "content"}}>
-                    {new Array(5).fill(null).map((_, index) => (
-                        <Collapse className={styles.collapse} key={index} ghost items={items}/>
-                    ))}
-                </Flex>
-            </Flex>
-        </div>
+                    <Flex vertical gap='var(--gap)' style={{maxWidth: 1200 - sortingBlockWidth}}>
+                        <Input placeholder="Пошук предметів" prefix={<SearchOutlined/>}
+                               onChange={e => setSearchText(e.target.value)}
+                               style={{position: "relative", flexGrow: 1, gridArea: "search",}}></Input>
+                        {/* todo when subjects will be in api */}
+                        <Flex vertical style={{background: 'var(--foreground)', borderRadius: 8, gridArea: "content"}}>
+                            {new Array(5).fill(null).map((_, index) => (
+                                <Collapse className={styles.collapse} key={index} ghost items={items}/>
+                            ))}
+                        </Flex>
+                    </Flex>
+                </div> : <div style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100%",
+                    fontSize: 24,
+                    color: "var(--text-secondary)"
+                }}>У вас немає доступу до цієї сторінки</div>
+            }
+        </>
     );
 }
