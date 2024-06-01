150b71936113ee331867b54f74847ffd88619923
Ivan Verba <ivanverba@192.168.0.106>
2024-06-01 16:10:14 +0300

    announced subject added things for teacher

--- src/app/(logged-in)/subject/announced/[slug]/page.tsx
+++ src/app/(logged-in)/subject/announced/[slug]/page.tsx
@@ -5,13 +5,87 @@ import { IUser, TemporaryAnnoncedSubjectInfo } from "@/types/api.types";
 import { notFound, useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-import { ProfileOutlined, LinkOutlined, CheckOutlined, ClockCircleOutlined, CalendarOutlined, FieldTimeOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { DeleteOutlined, ProfileOutlined, LinkOutlined, CheckOutlined, ClockCircleOutlined, CalendarOutlined, FieldTimeOutlined } from "@ant-design/icons";
+import { Button, Divider, Table, TableColumnsType, Tooltip } from "antd";
 
 import styles from "./styles.module.css";
 import Link from "next/link";
 import dayjs from "dayjs";
 import AnnouncedSubjectRequestModal from "@/components/modals/AnnouncedSubjectRequestModal";
+
+interface DataType {
+    key: React.Key,
+    firstNameAndLastName: string,
+    grade: number,
+}
+
+const MOCK_ANNOUNCED_SUBJECT_STUDENTS_DATA: DataType[] = [
+    {
+        key: '1',
+        firstNameAndLastName: "Іван Верба",
+        grade: 60
+    },
+    {
+        key: '2',
+        firstNameAndLastName: "Федір Остапчий",
+        grade: 80
+    },
+    {
+        key: '3',
+        firstNameAndLastName: "Галина Кущ",
+        grade: 30
+    }
+];
+
+const announcedSubjectStudentsColumns: TableColumnsType<DataType> = [
+    {
+        title: "Ім'я та фамілія",
+        dataIndex: "firstNameAndLastName",
+    },
+    {
+        title: "Бал",
+        dataIndex: "grade",
+        showSorterTooltip: { target: 'full-header' },
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.grade - b.grade,
+    },
+    {
+        title: "Операція",
+        width: 1,
+        render: (_, record) => {
+            return (
+                <Tooltip title="Відказати студенту бути в вашому предметі">
+                    <Button
+                        type="primary"
+                        shape="circle"
+                        danger
+                        icon={<DeleteOutlined />}
+                        style={{display: "block", margin: "auto"}}
+                    />
+                </Tooltip>
+            )
+        }
+    }
+];
+
+const ForTeacherRender = () => {
+    const examEnded = true;
+    return (
+        <>
+            <Divider />
+
+            {
+                !examEnded
+                ? <p style={{textAlign: "center"}}>Ви побачите список студентів, які подали заявку на вступ до вашого предмета і зможете видаляти тих, які на вашу думку не підходять, після того, як закінчиться іспит.</p>
+                : <Table
+                    dataSource={MOCK_ANNOUNCED_SUBJECT_STUDENTS_DATA}
+                    columns={announcedSubjectStudentsColumns}
+                    bordered
+                />
+            }
+        </>
+    );
+}
 
 export default function AnnouncedSubject() {
     const params = useParams();
@@ -23,6 +97,9 @@ export default function AnnouncedSubject() {
     const [isLoaded, setIsLoaded] = useState(false);
 
     const [modalVisible, setModalVisible] = useState(false);
+
+    /* Temporary */
+    const isTeacher = true;
 
     useEffect(() => {
         let subjectId = parseInt(slug as string);
@@ -90,7 +167,15 @@ export default function AnnouncedSubject() {
                     <p>{dayjs(info.start_date).format("D MMMM YYYY року")}</p>
                 </section>
 
-                <Button onClick={() => setModalVisible(true)} style={{display: "block", margin: "auto"}} type="primary">Подати заявку</Button>
+                {!isTeacher && 
+                    <Button onClick={() => setModalVisible(true)} style={{display: "block", margin: "auto"}} type="primary">
+                        {info.exam ? "Подати заявку" : "Зареєструвати мене"}
+                    </Button>
+                }
+
+                {isTeacher &&
+                    <ForTeacherRender />
+                }
            </div>
 
            <AnnouncedSubjectRequestModal
