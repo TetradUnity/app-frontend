c97a7b7bab3cc77620809c2229ed5e7783f72b86
hagoel-git <danilo.guk1@gmail.com>
2024-06-14 14:40:06 +0300

    fixed some stuff

--- src/components/usersPage.tsx
+++ src/components/usersPage.tsx
@@ -1,11 +1,12 @@
 'use client'
-import {Flex, Input, Space, Spin} from "antd";
+import {Button, Empty, Flex, Input, Space, Spin} from "antd";
 import UserCard from "@/components/cards/UserCard";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {IUser} from "@/types/api.types";
 import styles from "./users.module.css";
 import {UserService} from "@/services/user.service";
 import InfiniteScroll from "react-infinite-scroll-component";
+import {UpOutlined} from "@ant-design/icons";
 
 const pagination = 28;
 
@@ -19,10 +20,22 @@ export default function UsersPage({title, type}: UsersPageProps) {
     const [lastName, setLastName] = useState("");
     const [users, setUsers] = useState<IUser[]>([]);
 
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [hasMore, setHasMore] = useState(false);
 
     const [page, setPage] = useState(1);
+
+    const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 300) {
+                setShowScrollToTop(true);
+            } else {
+                setShowScrollToTop(false);
+            }
+        })
+    }, [])
 
     useEffect(() => {
         UserService.findUsers({
@@ -39,14 +52,18 @@ export default function UsersPage({title, type}: UsersPageProps) {
                 // @ts-ignore
                 setHasMore(response.data.length === pagination)
             }
-            console.log(response.data)
         })
+        setLoading(false);
     }, []);
 
     const loadMore = () => {
+        if (loading) return;
+
+        setLoading(true);
+
         UserService.findUsers({
-            first_name: firstName,
-            last_name: lastName,
+            first_name: firstName ? firstName.trim() : firstName,
+            last_name: lastName ? lastName.trim() : lastName,
             role: type,
             limit: pagination,
             page: page + 1
@@ -59,15 +76,19 @@ export default function UsersPage({title, type}: UsersPageProps) {
                 setHasMore(response.data.length === pagination)
             }
         })
+        setLoading(false);
     }
 
     const onSearch = () => {
+        if (loading) return;
+
+        setLoading(true);
         UserService.findUsers({
-            first_name: firstName,
-            last_name: lastName,
+            first_name: firstName ? firstName.trim() : firstName,
+            last_name: lastName ? lastName.trim() : lastName,
             role: type,
             limit: pagination,
-            page: 2
+            page: page
         }).then((response) => {
             if (response.success) {
                 // @ts-ignore
@@ -77,6 +98,7 @@ export default function UsersPage({title, type}: UsersPageProps) {
                 setHasMore(response.data.length === pagination)
             }
         })
+        setLoading(false);
     }
 
     return (
@@ -84,11 +106,13 @@ export default function UsersPage({title, type}: UsersPageProps) {
             <div className={styles.Header}>
                 <h3 style={{fontWeight: 450}}>{title}</h3>
                 <Space.Compact style={{width: "100%"}}>
-                    <Input placeholder="Прізвище" onChange={(e) => setFirstName(e.target.value)}/>
+                    <Input placeholder="Прізвище" onChange={(e) => setLastName(e.target.value)} onPressEnter={onSearch}
+                           style={{
+                               width: "calc(100% - 16px)"
+                           }}/>
                     <Input.Search placeholder="Ім'я" onChange={(e) => setFirstName(e.target.value)}
                                   onSearch={onSearch}/>
                 </Space.Compact>
-                {page}
             </div>
             <InfiniteScroll next={loadMore} hasMore={hasMore} loader={<Spin>Завантаження...</Spin>}
                             dataLength={users.length} style={{width: "100%"}}
@@ -99,13 +123,25 @@ export default function UsersPage({title, type}: UsersPageProps) {
                     gridTemplateColumns: 'repeat(auto-fill,minmax(153px,1fr))',
                     gridAutoRows: "auto",
                 }}>
-
                     {users.map((user, i) => (
                         <UserCard key={i} user={user}/>
                     ))}
-
                 </div>
             </InfiniteScroll>
+            {
+                (users.length === 0 && !loading) && <Empty description="Користувачів не найдено"></Empty>
+            }
+            <div className={`${styles.ScrollToTopButton} ${showScrollToTop ? styles.SlideUp : ''}`}>
+                <Button type="text" onClick={() => window.scrollTo({top: 0, behavior: "smooth"})}
+                        icon={<UpOutlined/>}
+                        style={{
+                            borderRadius: 50,
+                            width: 38,
+                            height: 38,
+                            backgroundColor: "#121212",
+                        }}/>
+            </div>
         </Flex>
+
     );
 }
